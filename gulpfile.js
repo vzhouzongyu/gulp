459e5ec824021b71b8245f560ee49d379ab2d812
vzhouzongyu <554877147@qq.com>
2016-04-01 16:45:02 +0800

    Signed-off-by: vzhouzongyu <554877147@qq.com>

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp'),
+	plugins = require('gulp-load-plugins')();
+
+require('./tasks/gulpfile-dev')(gulp, plugins)
+require('./tasks/gulpfile-build')(gulp, plugins)
+require('./tasks/gulpfile-publish')(gulp, plugins)
